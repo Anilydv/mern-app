be513a6d748c5765dafa7c59d7e5e53c3a895255
Anilydv <sanokanxa4516@gmail.com>
2021-06-09 14:36:29 +0545

    add app navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+} from "reactstrap";
 
-export default function Navbar() {
-    return <div>hello from navbar</div>;
+export default function DisplayNavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    const navMenu = () => {
+        return (
+            <>
+                <Navbar
+                    color="primary"
+                    light
+                    dark
+                    expand="md"
+                    style={{ padding: "10px 15px" }}
+                >
+                    <NavbarBrand href="/">Mern developer</NavbarBrand>
+                    <NavbarToggler onClick={toggle} />
+                    <Collapse
+                        isOpen={isOpen}
+                        navbar
+                        className=" justify-content-end"
+                    >
+                        <div className="amnil">
+                            <Nav className="mr-auto " navbar>
+                                <NavItem>
+                                    <NavLink href="/">Home</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/about">AboutMe</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/contact">Contact</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/login">Login</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/register">Register</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </div>
+                    </Collapse>
+                </Navbar>
+            </>
+        );
+    };
+
+    return <>{navMenu()}</>;
 }
