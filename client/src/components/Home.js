f111124c7e8b4a8a851ddfdf0a28ccbf3ea7b2fe
Anilydv <sanokanxa4516@gmail.com>
2021-06-14 16:09:40 +0545

    homepage design

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,32 @@
 import React from "react";
 
 export default function Home() {
-    return <div>welcome to homepage</div>;
+    return (
+        <div
+            style={{
+                background:
+                    "linear-gradient(to left, #def2f1  50%, #97d2d0 50%)",
+                height: "90vh",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <div style={{ textAlign: "center" }}>
+                <p
+                    style={{
+                        color: "blue",
+                        fontSize: "20px",
+                        fontWeight: 300,
+                        fontFamily: "math",
+                    }}
+                >
+                    WELCOME
+                </p>
+                <h1 style={{ fontWeight: 300, fontFamily: "fantasy" }}>
+                    We Are The MERN Developer
+                </h1>
+            </div>
+        </div>
+    );
 }
