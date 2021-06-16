2a4fcd4d831abeeca9bb2cd907dc211b843cdf14
Anilydv <sanokanxa4516@gmail.com>
2021-06-16 17:27:20 +0545

    authorized user access about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -65,16 +65,14 @@ const useStyles = makeStyles((theme) => ({
 
 export default function About() {
     const [showAbout, setShowAbout] = useState(true);
+    const [loading, setLoading] = useState(false);
     const classes = useStyles();
+    const history = useHistory();
 
     const handleApiCall = () => {
         return axios
             .get("/about", {
                 withCredentials: true,
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Content-Type": "application/json",
-                },
             })
             .then((res) => res)
             .catch((err) => {
@@ -83,25 +81,16 @@ export default function About() {
     };
 
     const callAboutPage = async () => {
-        // try {
-        //     const res = await handleApiCall();
-        //     console.log(res);
-        // } catch (error) {
-        //     console.log(error);
-        // }
-
         try {
-            const res = await fetch("/about", {
-                method: "GET",
-                headers: {
-                    Accept: "application/json",
-                    "Content-Type": "application/json",
-                },
-                credentials: "include",
-            });
-
-            const data = await res.json();
-            console.log(data);
+            const res = await handleApiCall();
+            if (res.status === 401) {
+                history.push("/login");
+                toast.error("Please make login ");
+                console.log("Unauthorized user");
+            } else {
+                setLoading(true);
+                console.log("authorized user");
+            }
         } catch (error) {
             console.log(error);
         }
@@ -111,139 +100,167 @@ export default function About() {
         callAboutPage();
     }, []);
 
-    return (
-        <div className={classes.root}>
-            <Paper className={classes.paper}>
-                <Grid container spacing={2}>
-                    <Grid item>
-                        <ButtonBase className={classes.image}>
-                            <img
-                                className={classes.userImg}
-                                alt="userImage"
-                                src={pic}
-                            />
-                        </ButtonBase>
-                        <div
-                            style={{
-                                marginLeft: "18px",
-                                marginTop: "15px",
-                                marginBottom: "15px",
-                                marginRight: "10px",
-                            }}
-                        >
-                            <Typography gutterBottom variant="subtitle1">
-                                WORK Link
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Developer
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Software Engineer
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Youtube
-                            </Typography>
-                        </div>
-                    </Grid>
-                    <Grid item xs={12} sm container>
-                        <Grid item xs container direction="column" spacing={2}>
-                            <Grid item xs>
+    const displayAboutPage = () => {
+        return (
+            <div className={classes.root}>
+                <Paper className={classes.paper}>
+                    <Grid container spacing={2}>
+                        <Grid item>
+                            <ButtonBase className={classes.image}>
+                                <img
+                                    className={classes.userImg}
+                                    alt="userImage"
+                                    src={pic}
+                                />
+                            </ButtonBase>
+                            <div
+                                style={{
+                                    marginLeft: "18px",
+                                    marginTop: "15px",
+                                    marginBottom: "15px",
+                                    marginRight: "10px",
+                                }}
+                            >
                                 <Typography gutterBottom variant="subtitle1">
-                                    anil kumar yadav
-                                </Typography>
-                                <Typography variant="body2" gutterBottom>
-                                    web Developer
+                                    WORK Link
                                 </Typography>
                                 <Typography
                                     variant="body2"
                                     color="textSecondary"
                                 >
-                                    RANKINGS : 1/10
+                                    Developer
                                 </Typography>
+                                <Typography
+                                    variant="body2"
+                                    color="textSecondary"
+                                >
+                                    Software Engineer
+                                </Typography>
+                                <Typography
+                                    variant="body2"
+                                    color="textSecondary"
+                                >
+                                    Youtube
+                                </Typography>
+                            </div>
+                        </Grid>
+                        <Grid item xs={12} sm container>
+                            <Grid
+                                item
+                                xs
+                                container
+                                direction="column"
+                                spacing={2}
+                            >
+                                <Grid item xs>
+                                    <Typography
+                                        gutterBottom
+                                        variant="subtitle1"
+                                    >
+                                        anil kumar yadav
+                                    </Typography>
+                                    <Typography variant="body2" gutterBottom>
+                                        web Developer
+                                    </Typography>
+                                    <Typography
+                                        variant="body2"
+                                        color="textSecondary"
+                                    >
+                                        RANKINGS : 1/10
+                                    </Typography>
+                                    <Button
+                                        variant="contained"
+                                        className={classes.buttonDesign}
+                                        onClick={() => setShowAbout(true)}
+                                    >
+                                        About
+                                    </Button>
+                                    <Button
+                                        variant="contained"
+                                        className={classes.buttonDesign}
+                                        onClick={() => setShowAbout(false)}
+                                    >
+                                        TimeLine
+                                    </Button>
+                                    <hr />
+                                    {showAbout && (
+                                        <div>
+                                            <Table>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>User Id</td>
+                                                        <td>
+                                                            3489129082349841
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Name</td>
+                                                        <td>
+                                                            Anil Kumar Yadav
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Email</td>
+                                                        <td>
+                                                            anilkumar@gmail.com
+                                                        </td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Phone</td>
+                                                        <td>9819997153</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Profession</td>
+                                                        <td>Web Developer</td>
+                                                    </tr>
+                                                </tbody>
+                                            </Table>
+                                        </div>
+                                    )}
+                                    {!showAbout && (
+                                        <div>
+                                            <Table>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Experience</td>
+                                                        <td>Expert</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Hourly Rate</td>
+                                                        <td>$10/hr</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Total Projects</td>
+                                                        <td>230</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>English Level</td>
+                                                        <td>Expert</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Availability</td>
+                                                        <td>6 months</td>
+                                                    </tr>
+                                                </tbody>
+                                            </Table>
+                                        </div>
+                                    )}
+                                </Grid>
+                            </Grid>
+                            <Grid item>
                                 <Button
                                     variant="contained"
-                                    className={classes.buttonDesign}
-                                    onClick={() => setShowAbout(true)}
+                                    className={classes.button}
                                 >
-                                    About
+                                    Edit Profile
                                 </Button>
-                                <Button
-                                    variant="contained"
-                                    className={classes.buttonDesign}
-                                    onClick={() => setShowAbout(false)}
-                                >
-                                    TimeLine
-                                </Button>
-                                <hr />
-                                {showAbout && (
-                                    <div>
-                                        <Table>
-                                            <tbody>
-                                                <tr>
-                                                    <td>User Id</td>
-                                                    <td>3489129082349841</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Name</td>
-                                                    <td>Anil Kumar Yadav</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Email</td>
-                                                    <td>anilkumar@gmail.com</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Phone</td>
-                                                    <td>9819997153</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Profession</td>
-                                                    <td>Web Developer</td>
-                                                </tr>
-                                            </tbody>
-                                        </Table>
-                                    </div>
-                                )}
-                                {!showAbout && (
-                                    <div>
-                                        <Table>
-                                            <tbody>
-                                                <tr>
-                                                    <td>Experience</td>
-                                                    <td>Expert</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Hourly Rate</td>
-                                                    <td>$10/hr</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Total Projects</td>
-                                                    <td>230</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>English Level</td>
-                                                    <td>Expert</td>
-                                                </tr>
-                                                <tr>
-                                                    <td>Availability</td>
-                                                    <td>6 months</td>
-                                                </tr>
-                                            </tbody>
-                                        </Table>
-                                    </div>
-                                )}
                             </Grid>
                         </Grid>
-                        <Grid item>
-                            <Button
-                                variant="contained"
-                                className={classes.button}
-                            >
-                                Edit Profile
-                            </Button>
-                        </Grid>
                     </Grid>
-                </Grid>
-            </Paper>
-        </div>
-    );
+                </Paper>
+            </div>
+        );
+    };
+
+    return <>{loading && displayAboutPage()}</>;
 }
