02eb2a2c9c742e2eda1a5d9884c169cfdfc61d7b
Anilydv <sanokanxa4516@gmail.com>
2021-06-17 00:13:21 +0545

    add dynamic data in homepage

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,6 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+    const [userName, setUserName] = useState("");
+
+    const handleApiCall = () => {
+        return axios
+            .get("/getData")
+            .then((res) => res)
+            .catch((err) => {
+                return err.response;
+            });
+    };
+
+    const callContactPage = async () => {
+        try {
+            const res = await handleApiCall();
+
+            setUserName(res.data.name);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        callContactPage();
+    }, []);
+
     return (
         <div
             style={{
@@ -16,16 +42,33 @@ export default function Home() {
                 <p
                     style={{
                         color: "blue",
-                        fontSize: "20px",
+                        fontSize: "16px",
                         fontWeight: 300,
                         fontFamily: "math",
+                        margin: "2px",
                     }}
                 >
                     WELCOME
                 </p>
-                <h1 style={{ fontWeight: 300, fontFamily: "fantasy" }}>
-                    We Are The MERN Developer
+                <h1
+                    style={{
+                        fontFamily: "monospace",
+                        fontWeight: "bold",
+                        fontSize: "50px",
+                        marginBottom: "18px",
+                    }}
+                >
+                    {userName}
                 </h1>
+                {userName ? (
+                    <h1 style={{ fontWeight: 300, fontFamily: "fantasy" }}>
+                        Happy, to see you back
+                    </h1>
+                ) : (
+                    <h1 style={{ fontWeight: 300, fontFamily: "fantasy" }}>
+                        We Are The MERN Developer
+                    </h1>
+                )}
             </div>
         </div>
     );
